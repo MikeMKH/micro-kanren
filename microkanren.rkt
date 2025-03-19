1bade9956b750b8203f80c4b5e6b3a7f5380d3b2
Mike Harris <MikeMKH@users.noreply.github.com>
2025-03-19 06:37:41 -0500

    add unify

--- microkanren.rkt
+++ microkanren.rkt
@@ -1,9 +1,38 @@
 #lang racket
 
+(define (var c) (vector c))
+(define (var? x) (vector? x))
+(define (var=? x1 x2) (= (vector-ref x1 0) (vector-ref x2 0)))
+
+(define (walk u s)
+  (let ((pr (and (var? u) (assq (lambda (v) (var=? u v)) s))))
+    (if pr (walk (cdr pr) s) u)))
+
+(define (ext-s x v s) `((,x . ,v) . ,s))
+
+(define (== u v)
+  (lambda (s/c)
+    (let ((s (unify u v (car s/c))))
+      (if s (unit `(,s . ,(cdr s/c))) mzero))))
+
+(define (unit s/c) (cons s/c mzero))
+(define mzero '())
+
+(define (unify u v s)
+  (let ((u (walk u s)) (v (walk v s)))
+    (cond
+      ((and (var? u) (var? v) (var=? u v)) s)
+      ((var? u) (ext-s u v s))
+      ((var? v) (ext-s v u s))
+      ((and (pair? u) (pair? v)
+       (let ((s (unify (car u) (car v) s)))
+         (and s (unify (cdr u) (cdr v) s)))))
+      (else (and (eqv? u v) s)))))
+
 (require (except-in rackunit fail))
 (require rackunit/text-ui)
 
 (run-tests
  (test-suite "example from paper"
-  (test-equal? "hello world" (cdr `(1 . a)) 'a)
+  (test-equal? "hello world" (unit 'hi) '(hi))
  ))
