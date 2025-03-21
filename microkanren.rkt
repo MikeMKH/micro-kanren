3fffaa68f4b238fea468f11f763c2de6b4f9b47c
Mike Harris <MikeMKH@users.noreply.github.com>
2025-03-21 05:51:25 -0500

    add 5.2-5.4

--- microkanren.rkt
+++ microkanren.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; code from http://webyrd.net/scheme-2013/papers/HemannMuKanren2013.pdf
+
 (define (var c) (vector c))
 (define (var? x) (vector? x))
 (define (var=? x1 x2) (= (vector-ref x1 0) (vector-ref x2 0)))
@@ -98,14 +100,70 @@
     ((_ (x0 x ...) g0 g ...)
      (call/fresh (lambda (x0) (fresh (x ...) g0 g ...))))))
 
+; 5.2
+(define (pull $) (if (procedure? $) (pull ($)) $))
+
+(define (take-all $)
+  (let (($ (pull $)))
+    (if (null? $) '() (cons (car $) (take-all (cdr $))))))
+
+(define (take n $)
+  (if (zero? n) '()
+      (let (($ (pull $)))
+        (cond
+          ((null? $) '())
+          (else (cons (car $) (take (- n 1) (cdr $))))))))
+
+; 5.3
+(define (mK-reify s/c*)
+  (map reify-state/1st-var s/c*))
+
+(define (reify-state/1st-var s/c)
+  (let ((v (walk* (var 0) (car s/c))))
+    (walk* v (reify-s v '()))))
+
+(define (reify-s v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v)
+       (let ((n (reify-name (length s))))
+         (cons `(,v . ,n) s)))
+      ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
+      (else s))))
+
+(define (reify-name n)
+  (string->symbol
+   (string-append "_" "." (number->string n))))
+
+(define (walk* v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v) v)
+      ((pair? v) (cons (walk* (car v) s)
+                       (walk* (cdr v) s)))
+      (else v))))
+
+; 5.4
+(define (call/empty-state g) (g empty-state))
+
+(define-syntax run
+  (syntax-rules ()
+    ((_ n (x ...) g0 g ...)
+     (mK-reify (take n (call/empty-state
+                        (fresh (x ...) g0 g ...)))))))
+
+(define-syntax run*
+  (syntax-rules ()
+    ((_ (x ...) g0 g ...)
+     (mK-reify (take-all (call/empty-state
+                          (fresh (x ...) g0 g ...)))))))
+
 (run-tests
- (test-suite "examples"
+ (test-suite "paper examples"
   (test-equal? "== q 5" ((call/fresh (lambda (q) (== q 5))) empty-state) '((((#(0) . 5)) . 1)))
   (test-equal? "a-and-b" (a-and-b empty-state)
     '((((#(1) . 5) (#(0) . 7)) . 2)
       (((#(1) . 6) (#(0) . 7)) . 2)))
-  ;(test-equal? "fives" ((call/fresh fives) empty-state) '((((#(0) . 5)) . endless))) ; endless loop, need take
   (test-equal? "five" ((call/fresh five) empty-state) '((((#(0) . 5)) . 1)))
-  ;(test-equal? "fives-and-sixes" (fives-and-sixes empty-state) '((((#(0) . 5)) . endless))) ; endless loop, need take
   (test-equal? "five-and-six" (five-and-six empty-state) '((((#(0) . 5)) . 1) (((#(0) . 6)) . 1)))
  ))
