6d29e2316e539be94ea1af627c8c61a133665c75
Mike Harris <MikeMKH@users.noreply.github.com>
2025-03-26 06:20:01 -0500

    add appendo example

---
+++ racket/example.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require "microkanren.rkt")
+(require "extension.rkt")
+
+;;; taken from https://github.com/jasonhemann/microKanren/blob/0f4505db0d2525fc3d567c5183e45f28992cfe72/miniKanren-wrappers.scm#L106-L121
+
+(define (appendo l s out)
+  (conde
+    ((== '() l) (== s out))
+    ((fresh (a d res)
+       (== `(,a . ,d) l)
+       (== `(,a . ,res) out)
+       (appendo d s res)))))
+
+(require (except-in rackunit fail))
+(require rackunit/text-ui)
+
+;;; I am not sure if the run* actual works
+
+(run-tests
+ (test-suite "examples"
+  (test-equal? "appendo x y '(1 2 3 4 5)"
+    (run* (q) (fresh (x y) (== `(,x ,y) q) (appendo x y '())))
+      '((() (1 2 3 4 5))
+        ((1) (2 3 4 5))
+        ((1 2) (3 4 5))
+        ((1 2 3) (4 5))
+        ((1 2 3 4) (5))
+        ((1 2 3 4 5) ())))
+ ))
